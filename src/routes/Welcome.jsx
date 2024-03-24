980723f271bb2a181d9980fe669f8fbedd325e59
Ries <164700045+RiesVR@users.noreply.github.com>
2024-03-24 02:33:12 +0100

    Update Welcome.jsx

--- src/routes/Welcome.jsx
+++ src/routes/Welcome.jsx
@@ -7,10 +7,10 @@ const Welcome = () => {
                 <img src={logo} className="logo" alt="logo" />
             </header>
             <p>
-                Welcome to the KDRework Rules. Here you can find all the rules for the KDRework server.
+                Welcome to the KDRework. On the left site you have some buttons to read the rules.
             </p>
             <p>
-                If you have any further questions, please contact us on our Discord server.
+                If you have any further questions, please contact an Administrator.
             </p>
         </>
     );
